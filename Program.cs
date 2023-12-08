c966045930275ca103cd4e1f4a781b9f16ce033a
Shykoz <roma.chekmak123@gmail.com>
2023-12-08 12:51:47 +0200

    PowMethod

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowMethod(5, 4));
+        }
+        public static int PowMethod(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result = result * a;
+            }
+            return result;
+        }
+    }
+}
